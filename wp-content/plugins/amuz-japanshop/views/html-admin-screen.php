afc04233bc4c295fa5bf06760b5b24c8de0fd156
zoozkwon <zooz@amzu.co.kr>
2018-07-10 16:58:20 +0900

    미완성 정산탭 제거

--- wp-content/plugins/amuz-japanshop/views/html-admin-screen.php
+++ wp-content/plugins/amuz-japanshop/views/html-admin-screen.php
@@ -3,6 +3,7 @@
         <a href="<?php echo admin_url('admin.php?page=wc4amuz_japanshop_datacenter_output') ?>" class="nav-tab <?php echo ($tab == 'data') ? 'nav-tab-active' : ''; ?>"><?php echo __( '커스텀 주문데이터', 'amuz-japanshop' )?></a>
         <a href="<?php echo admin_url('admin.php?page=wc4amuz_japanshop_datacenter_output&tab=product') ?>" class="nav-tab <?php echo ($tab == 'product') ? 'nav-tab-active' : ''; ?>"><?php echo __( '상품데이터 추출', 'amuz-japanshop' )?></a>
     </h2>
+
 	<?php
 		switch ($tab) {
 			case "product" :
