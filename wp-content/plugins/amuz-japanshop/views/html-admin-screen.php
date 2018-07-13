f7ef6cf4dba0f9e3f8ca2fe70df1dd078d6aba2d
zoozkwon <zooz@amzu.co.kr>
2018-07-13 16:05:34 +0900

    데이터센터 정산표

--- wp-content/plugins/amuz-japanshop/views/html-admin-screen.php
+++ wp-content/plugins/amuz-japanshop/views/html-admin-screen.php
@@ -2,13 +2,17 @@
     <h2 class="nav-tab-wrapper woo-nav-tab-wrapper">
         <a href="<?php echo admin_url('admin.php?page=wc4amuz_japanshop_datacenter_output') ?>" class="nav-tab <?php echo ($tab == 'data') ? 'nav-tab-active' : ''; ?>"><?php echo __( '커스텀 주문데이터', 'amuz-japanshop' )?></a>
         <a href="<?php echo admin_url('admin.php?page=wc4amuz_japanshop_datacenter_output&tab=product') ?>" class="nav-tab <?php echo ($tab == 'product') ? 'nav-tab-active' : ''; ?>"><?php echo __( '상품데이터 추출', 'amuz-japanshop' )?></a>
-
+        <a href="<?php echo admin_url('admin.php?page=wc4amuz_japanshop_datacenter_output&tab=calculate') ?>" class="nav-tab <?php echo ($tab == 'calculate') ? 'nav-tab-active' : ''; ?>"><?php echo __( '정산', 'amuz-japanshop' )?></a>
     </h2>
 	<?php
 		switch ($tab) {
 			case "product" :
                 include( 'html-admin-product-screen.php' );
 			break;
+
+            case "calculate" :
+                include( 'html-admin-calculate-screen.php' );
+                break;
 
 			default :
                 include( 'html-admin-datacenter-screen.php' );
