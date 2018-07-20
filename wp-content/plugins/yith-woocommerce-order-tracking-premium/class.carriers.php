85b46122074debeeadacecf02fff5b7d52787679
zoozkwon <zooz@amzu.co.kr>
2018-07-20 13:57:59 +0900

    배송 상황 조회

--- wp-content/plugins/yith-woocommerce-order-tracking-premium/class.carriers.php
+++ wp-content/plugins/yith-woocommerce-order-tracking-premium/class.carriers.php
@@ -521,7 +521,7 @@ if ( ! class_exists( 'Carriers' ) ) {
 				),
 				'SAGAWA_EX'         => array(
 					'name'      => 'SAGAWA EXPRESS',
-					'track_url' => 'https://www.aftership.com/couriers/sagawa/[TRACK_CODE]',
+					'track_url' => 'https://track.aftership.com/sagawa/[TRACK_CODE]',
 				),
 				'TOLLGROUP'         => array(
 					'name'      => 'Toll Priority | Toll Group',
