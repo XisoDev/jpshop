77ab158a945ec396367fbaa7fcfff8530c1a04ba
zoozkwon <zooz@amzu.co.kr>
2018-08-07 10:54:11 +0900

    savefile back

---
+++ wp-content/plugins/amuz-japanshop/actions/savefile.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * In WordPress Administration Screens
+ *
+ * @since 2.3.2
+ */
+if ( ! defined( 'WP_ADMIN' ) ) {
+    define( 'WP_ADMIN', true );
+}
+if ( ! defined('WP_NETWORK_ADMIN') )
+    define('WP_NETWORK_ADMIN', false);
+if ( ! defined('WP_USER_ADMIN') )
+    define('WP_USER_ADMIN', false);
+if ( ! WP_NETWORK_ADMIN && ! WP_USER_ADMIN ) {
+    define('WP_BLOG_ADMIN', true);
+}
+if ( isset($_GET['import']) && !defined('WP_LOAD_IMPORTERS') )
+    define('WP_LOAD_IMPORTERS', true);
+require_once('../../../../wp-load.php');
+require_once('../define_arrays.php');
+nocache_headers();
+$order_list = $_POST['cart'];
+$saver = array();
+if($_POST['action'] == "sagawa"){
+    include('./sagawa.php');
+}else if($_POST['action'] == "tqoon_orderlist") {
+    include('./tqoon_orderlist.php');
+}else if($_POST['action'] == "tqoon_packing") {
+    include('./tqoon_packing.php');
+}else if($_POST['action'] == "tqoon_orderlist"){
+    include('./tqoon_orderlist.php');
+}
+/*
+else if($_POST['action'] == "calculate") {
+    include('./calculate.php');
+}
+*/
+?>
