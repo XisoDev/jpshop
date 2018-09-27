e1983729620ca79e17813fcb17a74343505c5422
zoozkwon <zooz@amzu.co.kr>
2018-09-27 17:36:01 +0900

    정산표 수치 조정

--- wp-content/plugins/amuz-japanshop/views/html-admin-calculate-screen.php
+++ wp-content/plugins/amuz-japanshop/views/html-admin-calculate-screen.php
@@ -343,8 +343,8 @@ if ($payment == '편의점') {
             elseif ($zeusm < 300000) $pgm_tax = 600;
         } elseif ($payment == '신용카드'){
             if($card_type=='visa'||$card_type=='mastercard')
-                $pgm_tax = $zeusm * 3.35 / 100;
-            else $pgm_tax = $zeusm * 2.85 / 100;
+                $pgm_tax = $zeusm * 2.85 / 100;
+            else $pgm_tax = $zeusm * 3.35 / 100;
         }
         elseif ($payment == '은행결제') $pgm_tax = ($zeusm * 1.50) / 100;
         elseif ($payment == '대인결제'){
