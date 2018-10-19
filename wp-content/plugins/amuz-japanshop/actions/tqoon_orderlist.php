81ec1db19c066efe1dc58e0feb810884154e422d
zoozkwon <zooz@amzu.co.kr>
2018-10-19 15:18:40 +0900

    오더리스트 수정

--- wp-content/plugins/amuz-japanshop/actions/tqoon_orderlist.php
+++ wp-content/plugins/amuz-japanshop/actions/tqoon_orderlist.php
@@ -95,7 +95,6 @@ foreach($order_list as $no => $order_id){
     ##총 결제액
     $objPHPExcel->getActiveSheet()->setCellValue("H" . ($no+2),($item_subtotal + $fee_totals + $total_Convenience + $shipping));
 
-    print_r($order);
     if($order->get_meta('평균 관세율')!="")
     $perper = $order->get_meta('평균 관세율');
     else
