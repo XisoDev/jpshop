e2767fccb379e350bdddbee93893f6990edd527f
zoozkwon <zooz@amzu.co.kr>
2018-10-24 11:25:00 +0900

    custom fee

--- wp-content/plugins/amuz-japanshop/actions/tqoon_orderlist.php
+++ wp-content/plugins/amuz-japanshop/actions/tqoon_orderlist.php
@@ -95,11 +95,9 @@ foreach($order_list as $no => $order_id){
     ##총 결제액
     $objPHPExcel->getActiveSheet()->setCellValue("H" . ($no+2),($item_subtotal + $fee_totals + $total_Convenience + $shipping));
 
-    print_r($order);
     if($order->get_meta('평균 관세율')!="")
     $perper = $order->get_meta('평균 관세율');
-    else
-        $perper = "준비 중";
+    else $perper = "준비 중";
   /*  $oHSInfo = getHsValues($hs_codes, $order->get_items()); */
 /*
     $oHSInfo['tqoon_per'];
