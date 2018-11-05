71184d035eff91c122c1677d4520863b17868378
zoozkwon <zooz@amzu.co.kr>
2018-11-05 14:01:56 +0900

    delete success_rate

--- wp-content/plugins/indeed-affiliate-pro/public/views/account_page-list_visits.php
+++ wp-content/plugins/indeed-affiliate-pro/public/views/account_page-list_visits.php
@@ -12,9 +12,11 @@
 		<div class="uapcol-md-3 uap-account-visits-tab2">
 			<div class="uap-account-no-box uap-account-box-blue"><div class="uap-account-no-box-inside"><div class="uap-count"><?php echo $data['stats']['conversions'];?></div><div class="uap-detail"><?php _e('Conversions', 'uap');?></div></div></div>
 		</div>
+        <?php /*?>
 		<div class="uapcol-md-3 uap-account-visits-tab3">
 			<div class="uap-account-no-box uap-account-box-red"><div class="uap-account-no-box-inside"><div class="uap-count"><?php echo $data['stats']['success_rate'] . ' %';?></div><div class="uap-detail"><?php _e('Success Rate', 'uap');?></div></div></div>
 		</div>
+        <?php */?>
 	</div>
 	<?php echo $data['filter'];?>
 	<?php if (!empty($data['items']) && is_array($data['items'])):?>
@@ -24,7 +26,7 @@
 					<th class="url"><?php _e("Reffering URL", 'uap');?></th>
 					<th class="browser"><?php _e("Browser", 'uap');?></th>
 					<th class="device"><?php _e("Device", 'uap');?></th>
-					<th><?php _e("Date", 'uap');?></th>
+					<th class="date"><?php _e("Date", 'uap');?></th>
 					<th><?php _e("Converted", 'uap');?></th>
 				</tr>
 			</thead>
@@ -33,7 +35,7 @@
 					<th class="url"><?php _e("Reffering URL", 'uap');?></th>
 					<th class="browser"><?php _e("Browser", 'uap');?></th>
 					<th class="device"><?php _e("Device", 'uap');?></th>
-					<th><?php _e("Date", 'uap');?></th>
+					<th class="date"><?php _e("Date", 'uap');?></th>
 					<th><?php _e("Converted", 'uap');?></th>
 				</tr>
 			</tfoot>
@@ -43,7 +45,7 @@
 					<td  class="url"><a href="<?php echo $array['url'];?>" target="_blank"><?php echo $array['url'];?></a></td>
 					<td  class="browser"><?php echo $array['browser'];?></td>
 					<td  class="device"><?php echo $array['device'];?></td>
-					<td><?php echo uap_convert_date_to_us_format($array['visit_date']);?></td>
+					<td  class="date"><?php echo uap_convert_date_to_us_format($array['visit_date']);?></td>
 					<td class="uap-special-label" style="text-align:center;"><?php 
 						if ($array['referral_id']) echo 'Yes';
 						else echo 'No';
